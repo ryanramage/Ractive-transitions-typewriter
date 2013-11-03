54de944c36bd4f439e7592b2c1929dd0853a8471
Rich Harris <richard.a.harris@gmail.com>
2013-11-03 13:07:18 +0000

    wrong name

--- src/Ractive-transitions-typewriter.js
+++ src/Ractive-transitions-typewriter.js
@@ -58,7 +58,7 @@
 	}
 
 	else {
-		throw new Error( 'Could not find Ractive! It must be loaded before the Ractive-transitions-fly plugin' );
+		throw new Error( 'Could not find Ractive! It must be loaded before the Ractive-transitions-typewriter plugin' );
 	}
 
 }( typeof window !== 'undefined' ? window : this, function ( Ractive ) {
